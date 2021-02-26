8376451df6189211f1f42b284acf0133d634defa
n-desantis92 <dsnwebdev@gmail.com>
2021-02-26 14:55:52 +0100

    inseriti primi dati js

---
+++ script/script.js
@@ -0,0 +1,6 @@
+//domando il numero di kilometri da percorrere
+var kilometri = prompt("inserisci i kilometri da percorrere");
+//domando l'età
+var età = prompt("inserisci la tua età");
+//definisco il prezzo al kilometro
+var prezzokm = 0.21;
